f7ad3340356e4f8693c5c80a3ae3b4085bcedf73
MarcoHartmann <marcohartmann95@gmail.com>
2018-08-12 23:31:47 +0200

    Add function that gets status from nunchuk

--- i2c_nunchuk.c
+++ i2c_nunchuk.c
@@ -9,6 +9,50 @@
 * i2c DRIVER
 *******************************************************************************/
 
+static int get_status(struct i2c_client *cl)
+{
+	int err, count;
+	char buf[6];
+
+	/**
+	 * Read Status:
+	 * -wait for 10ms (spacing to previous i2c command)
+	 * -write 0x00
+	 * -wait for 10ms
+	 * -read 6bytes
+	 */
+	msleep(10);
+
+	buf[0] = 0x00;
+	count = 1;
+	err = i2c_master_send(cl, buf, count);
+	if (err != count)
+		goto write_err;
+
+	msleep(10);
+
+	count = 6;
+	err = i2c_master_recv(cl, buf, count);
+	if (err != count)
+		goto read_err;
+
+
+	/* TODO: parse buffer */
+	pr_alert("%x,%x,%x,%x,%x,%x\n",
+		buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+
+	return 0;
+
+/* error handling */
+write_err:
+	dev_err(&cl->dev, "%s: i2c write error (err=%d)\n", __func__, err);
+	return err;
+
+read_err:
+	dev_err(&cl->dev, "%s: i2c read error (err=%d)\n", __func__, err);
+	return err;
+}
+
 static int nunchuk_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 {
 	int err, count;
@@ -35,7 +79,7 @@ static int nunchuk_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	count = 2;
 	err = i2c_master_send(cl, buf, count);
 	if (err != count)
-		goto trans_err;
+		goto write_err;
 
 	msleep(1);
 
@@ -43,14 +87,22 @@ static int nunchuk_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	buf[1] = 0x00;
 	err = i2c_master_send(cl, buf, count);
 	if (err != count)
-		goto trans_err;
+		goto write_err;
 
+	/* read status */
+	err = get_status(cl);
+	if (err)
+		goto status_err;
 
 	return 0;
 
 /* error handling */
-trans_err:
-	dev_err(&cl->dev, "%s: Transmission Error (err=%d)\n", __func__, err);
+write_err:
+	dev_err(&cl->dev, "%s: i2c write error (err=%d)\n", __func__, err);
+	return err;
+
+status_err:
+	dev_err(&cl->dev, "%s: could not get status from nunchuk\n", __func__);
 	return err;
 }
 
