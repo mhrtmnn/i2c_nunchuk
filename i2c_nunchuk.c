c79d8d0292c8c15c257850be08cbe6ae051f3b62
MarcoHartmann <marcohartmann95@gmail.com>
2018-08-12 22:26:48 +0200

    Add i2c drv skeleton

--- i2c_nunchuk.c
+++ i2c_nunchuk.c
@@ -1,21 +1,77 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/i2c.h>
 
 
-static int __init hello_start(void)
+/*******************************************************************************
+* i2c DRIVER
+*******************************************************************************/
+
+static int nunchuk_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 {
-		printk(KERN_INFO "Loading hello module...\n");
-		printk(KERN_INFO "Hello world\n");
-		return 0;
+	dev_info(&cl->dev, "%s called\n", __func__);
+	return 0;
 }
-module_init(hello_start);
 
-static void __exit hello_end(void)
+static int nunchuk_remove(struct i2c_client *cl)
 {
-		printk(KERN_INFO "Goodbye Mr.\n");
+	dev_info(&cl->dev, "%s called\n", __func__);
+	return 0;
 }
-module_exit(hello_end);
+
+
+/*******************************************************************************
+* i2c DRIVER
+*******************************************************************************/
+
+static const struct of_device_id nunchuk_of_match[] = {
+		{ .compatible = "nintendo,nunchuk" },
+		{}
+};
+MODULE_DEVICE_TABLE(of, nunchuk_of_match);
+
+/* fallback to OF style bus device match */
+static const struct i2c_device_id nunchuk_id[] = {
+	{"nunchuk", 0},
+	{}
+};
+MODULE_DEVICE_TABLE(i2c, nunchuk_id);
+
+static struct i2c_driver nunchuk_drv = {
+	.driver = {
+		.name = "nunchuk-driver",
+		.owner = THIS_MODULE,
+		.of_match_table = nunchuk_of_match
+	},
+	.probe = nunchuk_probe,
+	.remove = nunchuk_remove,
+	.id_table = nunchuk_id,
+};
+
+
+/*******************************************************************************
+* MODULE HOOKS
+*******************************************************************************/
+
+static int __init nunchuk_start(void)
+{
+	printk(KERN_INFO "Loading module %s\n", THIS_MODULE->name);
+	return i2c_add_driver(&nunchuk_drv);
+}
+module_init(nunchuk_start);
+
+static void __exit nunchuk_end(void)
+{
+	printk(KERN_INFO "Unloading module %s\n", THIS_MODULE->name);
+	i2c_del_driver(&nunchuk_drv);
+}
+module_exit(nunchuk_end);
+
+
+/*******************************************************************************
+* MODULE INFORMATION
+*******************************************************************************/
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Marco Hartmann");
